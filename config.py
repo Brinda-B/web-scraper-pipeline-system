108614410b2e6e3ed33126add67a5d8e0bd92bef
Brinda B <23bca010@caias.in>
2026-06-23 11:15:12 +0530

    Add DB config setup

---
+++ config.py
@@ -0,0 +1,6 @@
+
+DB_HOST = "localhost"
+DB_NAME = "scraper_db"
+DB_USER = "postgres"
+DB_PASSWORD = "brinda@2005"
+DB_PORT = 5432
--- database/db.py
+++ database/db.py
@@ -1,14 +1,12 @@
 import psycopg2
-import os
-from dotenv import load_dotenv
+import config
 
-load_dotenv()
 
 def get_connection():
     return psycopg2.connect(
-        host=os.getenv("DB_HOST"),
-        database=os.getenv("DB_NAME"),
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASSWORD"),
-        port=os.getenv("DB_PORT")
+        host=config.DB_HOST,
+        database=config.DB_NAME,
+        user=config.DB_USER,
+        password=config.DB_PASSWORD,
+        port=config.DB_PORT
     )
